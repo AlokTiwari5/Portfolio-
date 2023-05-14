fbdb4537cb7352b6bc52fe3a15588197447bf157
Alok Kumar Tiwari <123202612+AlokTiwari5@users.noreply.github.com>
2023-05-14 16:01:11 +0530

    app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+	mobile_menu.classList.toggle('active'); 
 });
 
 document.addEventListener('scroll', () => {
